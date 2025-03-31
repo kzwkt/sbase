284bc7ea7de2ab4cca9f48fdd73c7213d83c150d
kzwkt <9437935+kzwkt@users.noreply.github.com>
2025-03-31 16:40:16 +0545

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@ git clone --depth=1 https://git.suckless.org/sbase/
 cp config.mk  sbase/
 cp Makefile  sbase/
 cd sbase
-scripts/getconf.sh > getconf.h
+make getconf.h
 make DESTDIR="$PWD/pkg" sbase-box-install
 tar -czvf sbase-static.tar.gz -C "$PWD/pkg" .
 

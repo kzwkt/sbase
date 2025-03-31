2be12e36e8f3b9cf634748334e91eae11c2061ab
kzwkt <9437935+kzwkt@users.noreply.github.com>
2025-03-31 10:20:38 +0545

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,3 @@
-PREFIX = /usr/
-CC = musl-gcc
-CFLAGS = '-static -Os -fPIE -pipe'
-LDFLAGS = '-static -static-pie'
-
 git clone --depth=1 https://git.suckless.org/sbase/
 
 # make all install uninstall dist sbase-box-install sbase-box-uninstall clean

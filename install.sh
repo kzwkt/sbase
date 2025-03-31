a78cf17469b9bbf1c8a57733717fc6e5f01807c8
kzwkt <9437935+kzwkt@users.noreply.github.com>
2025-03-31 10:30:10 +0545

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,7 @@
 sudo apt install musl-tools -y
 git clone --depth=1 https://git.suckless.org/sbase/
+cp config.mk  sbase/
+make dist
 
 # make all install uninstall dist sbase-box-install sbase-box-uninstall clean
 # links to /lib/x86_64-linux-gnu/libc.so if build from debian

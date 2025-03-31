88a7601900e7368e7f1ac5a7a0eaa170a590cce7
kzwkt <9437935+kzwkt@users.noreply.github.com>
2025-03-31 14:44:18 +0545

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,8 +2,7 @@ sudo apt install musl-tools -y
 git clone --depth=1 https://git.suckless.org/sbase/
 cp config.mk  sbase/
 cd sbase
-make dist
-make DESTDIR="$PWD/pkg" install
+make DESTDIR="$PWD/pkg" sbase-box-install
 tar -czvf sbase-static.tar.gz -C "$PWD/pkg" .
 
 # make all install uninstall dist sbase-box-install sbase-box-uninstall clean
